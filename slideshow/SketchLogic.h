f211a933afdf7873d577ae5175c1fd5cc5dc4a10
jimmythefung <jimmythefung@gmail.com>
2022-10-04 01:17:34 -0400

    bugfix

--- slideshow/SketchLogic.h
+++ slideshow/SketchLogic.h
@@ -1,165 +1,143 @@
+#include "pins_arduino.h"
 /*
  * 8x8 LED and Driver parameters
  */
 int DIN = 5;  // DIN pin of MAX7219 module
 int CLK = 4;  // CLK pin of MAX7219 module
-int CS  = 6;  // CS pin of MAX7219 module
+int CS = 6;   // CS pin of MAX7219 module
 int maxInUse = 1;
 MaxMatrix m(DIN, CS, CLK, maxInUse);
 
 // sketch parameters
-int frameSize = 0;
-char frameBuffer[10][10] = {0};
-char sketch[8][8] = {0};
+int currDeckSize = 0;
+int maxDeckSize = 9;
+char slideDeck[10][10] = { 0 };
+char sketch[8][8] = { 0 };
 int mode = 0;
-const int modeLEDPin = 13;
+const int modeLEDPin = LED_BUILTIN;
+static int save_count = 0;
+static int toggle_count = 0;
 
-char reduceCharArr(char a[])
-{
-    char c = 0;
-    for (int i = 0; i < 8; i++)
-    {
-        if (a[i] == 1)
-        {
-            c = c | (1 << 7 - i);
-        }
+char arrTo8BitsChar(char a[]) {
+  // Example: a={1,0,0,0,1,0,0,0} -> c='1000100'
+  char c = 0;
+  for (int i = 0; i < 8; i++) {
+    if (a[i] == 1) {
+      c = c | (1 << 7 - i);
     }
-    return c;
+  }
+  return c;
 }
 
-void rotateSketchBy90(char a[8][8])
-{
-    char t[8][8] = {0};
-    // construct rotateSketchBy90; first row is last column
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            t[j][7 - i] = a[i][j];
-        }
+void rotateSketchBy90(char sketch[8][8]) {
+  char tmp[8][8] = { 0 };
+  // construct rotateSketchBy90; first row is last column
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      tmp[j][7 - i] = sketch[i][j];
     }
+  }
 
-    // copy the rotateSketchBy90 back to input arr
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            a[i][j] = t[i][j];
-        }
+  // copy the rotateSketchBy90 back to input arr
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      sketch[i][j] = tmp[i][j];
     }
+  }
 }
 
-void saveSketchToBuffer(char sketch[8][8], char bufferCell[10])
-{
-    rotateSketchBy90(sketch);
-
-    // first 2 rows are static dimensions 8x8
-    bufferCell[0] = 8;
-    bufferCell[1] = 8;
-
-    // write each row to frame
-    for (int i = 0; i < 8; i++)
-    {
-        bufferCell[i + 2] = reduceCharArr(sketch[i]);
+bool sketchIsNotBlank(char sketch[8][8]) {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (sketch[i][j] == 1) {
+        return true;
+      }
     }
+  }
+  return false;
 }
 
-void printSketch(char arr[8][8])
-{
+void saveSketchToASlide(char sketch[8][8], char slideFrame[10]) {
+  rotateSketchBy90(sketch);
 
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            if (arr[i][j] == 1)
-            {
-                Serial.print(1);
-            }
-            else
-            {
-                Serial.print(0);
-            }
-            Serial.print(" ");
-        }
-        Serial.print("\n");
-    }
+  // first 2 rows are static dimensions 8x8
+  slideFrame[0] = 8;
+  slideFrame[1] = 8;
+
+  // write each row to frame
+  for (int i = 0; i < 8; i++) {
+    slideFrame[i + 2] = arrTo8BitsChar(sketch[i]);
+  }
 }
 
-void updateCursorPosition(int &c, int &r)
-{
-    // Right
-    if (movedRight())
-    {
-        Serial.println("Right (+X)");
-        if (c < 7)
-        {
-            c++;
-        }
+void printSketch(char sketch[8][8]) {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if (sketch[i][j] == 1) {
+        Serial.print(1);
+      } else {
+        Serial.print(0);
+      }
+      Serial.print(" ");
     }
-
-    // Left
-    if (movedLeft())
-    {
-        Serial.println("Left (-X)");
-        if (c > 0)
-        {
-            c--;
-        }
-    }
-
-    // Up
-    if (movedUp())
-    {
-        Serial.println("Down (-Y)");
-        if (r < 7)
-        {
-            r++;
-        }
-    }
-
-    // Down
-    if (movedDown())
-    {
-        Serial.println("Up (+Y)");
-        if (r > 0)
-        {
-            r--;
-        }
-    }
-}
-
-void mode_toggle()
-{
-    Serial.print("Mode toggled. Before: ");
-    Serial.print(mode);
     Serial.print("\n");
-    if (pin2ButtonReleased())
-    {
-        // toggle mode and LED
-        mode ^= 1;
-        digitalWrite(modeLEDPin, mode);
-    }
-    Serial.print("After: ");
-    Serial.print(mode);
-    Serial.print("\n");
+  }
 }
 
-void saveSketch()
-{
-    Serial.print("Save sketch button.\n");
-    if (pin3ButtonReleased())
-    {
-        Serial.print("Saved.\n");
-        if (frameSize < 9)
-        {
-            frameSize++;
-            saveSketchToBuffer(sketch, frameBuffer[frameSize - 1]);
-            // clear sketch screen
-            memset(sketch, 0, sizeof(sketch));
-        }
-        Serial.print("Frame size: ");
-        Serial.println(frameSize);
-    }else{
-      Serial.print("Not Saved.\n");
+void updateCursorPosition(int &c, int &r) {
+  // Right
+  if (movedRight()) {
+    Serial.println("Right (+X)");
+    if (c < 7) {
+      c++;
     }
+  }
+
+  // Left
+  if (movedLeft()) {
+    Serial.println("Left (-X)");
+    if (c > 0) {
+      c--;
+    }
+  }
+
+  // Up
+  if (movedUp()) {
+    Serial.println("Down (-Y)");
+    if (r < 7) {
+      r++;
+    }
+  }
+
+  // Down
+  if (movedDown()) {
+    Serial.println("Up (+Y)");
+    if (r > 0) {
+      r--;
+    }
+  }
+}
+
+void modeToggle() {
+  mode ^= 1;
+  digitalWrite(modeLEDPin, mode);
+
+  // stdout
+  Serial.print("Mode: ");
+  Serial.println(mode);
+}
+
+void saveSketch() {
+  if (currDeckSize < maxDeckSize && sketchIsNotBlank(sketch)) {
+    currDeckSize++;
+    saveSketchToASlide(sketch, slideDeck[currDeckSize - 1]);
+
+    // clear sketch screen
+    memset(sketch, 0, sizeof(sketch));
+
+    // stdout
+    Serial.print("Saved sketch. ");
+    Serial.print("Slide deck: ");
+    Serial.println(currDeckSize);
+  }
 }
--- slideshow/slideshow.ino
+++ slideshow/slideshow.ino
@@ -1,122 +1,102 @@
 #include <MaxMatrix.h>
 #include "JoyStick.h"
-#include "Buttons.h"
 #include "SketchLogic.h"
 
-void refreshScreen(char sketch[8][8])
-{
-    for (int r = 0; r < 8; r++)
-    {
-        for (int c = 0; c < 8; c++)
-        {
-            if (sketch[r][c] == 1)
-            {
-                m.setDot(c, r, true);
-            }
-            else
-            {
-                m.setDot(c, r, false);
-            }
-        }
+const int pin2Button = 2;  // The mode-toggle push button
+const int pin3Button = 3;  // The save sketch push button
+
+void refreshScreen(char sketch[8][8]) {
+  for (int r = 0; r < 8; r++) {
+    for (int c = 0; c < 8; c++) {
+      if (sketch[r][c] == 1) {
+        m.setDot(c, r, true);
+      } else {
+        m.setDot(c, r, false);
+      }
     }
+  }
 }
 
-void manage_display()
-{
-    static int c = 4;
-    static int r = 4;
+void manage_display() {
+  static int c = 4;
+  static int r = 4;
 
-    // blink cursor
-    m.setDot(c, r, true);
-    delay(50);
-    m.setDot(c, r, false);
-    delay(50);
+  // blink cursor
+  m.setDot(c, r, true);
+  delay(50);
+  m.setDot(c, r, false);
+  delay(50);
 
-    // user may have moved the cursor
-    updateCursorPosition(c, r);
+  // user may have moved the cursor
+  updateCursorPosition(c, r);
 
-    // user may have saved a pixel
-    if (joystick_button_is_released())
-    {
-        if (sketch[r][c] == 0)
-        {
-            sketch[r][c] = 1;
-        }
-        else
-        {
-            sketch[r][c] = 0;
-        }
-
-        Serial.print("Saving pixels: ");
-        Serial.print("R:");
-        Serial.print(r);
-        Serial.print(" C:");
-        Serial.print(c);
-        Serial.print("\n");
-        printSketch(sketch);
+  // user may have saved a pixel
+  if (joystick_button_is_released()) {
+    if (sketch[r][c] == 0) {
+      sketch[r][c] = 1;
+    } else {
+      sketch[r][c] = 0;
     }
 
-    // Maintain sketch on screen
-    refreshScreen(sketch);
+    Serial.print("Saving pixels: ");
+    Serial.print("R:");
+    Serial.print(r);
+    Serial.print(" C:");
+    Serial.print(c);
+    Serial.print("\n");
+    printSketch(sketch);
+  }
+
+  // Maintain sketch on screen
+  refreshScreen(sketch);
 }
 
-void scrollSprite(const byte *sprite)
-{
-    m.clear();
-    m.writeSprite(8, 0, sprite);
-    for (int i = 0; i < 16; i++)
-    {
-        if (mode == 1)
-        {
-            m.shiftLeft(false, false);
-            delay(readPot());
-        }
+void scrollSprite(const byte *sprite) {
+  m.clear();
+  m.writeSprite(8, 0, sprite);
+  for (int i = 0; i < 16; i++) {
+    if (mode == 1) {
+      m.shiftLeft(false, false);
+      delay(readPot());
     }
+  }
 }
 
-void scrollBuffer(char frameBuffer[10][10])
-{
-    for (int n = 0; n < frameSize; n++)
-    {
-        scrollSprite(frameBuffer[n]);
-    }
+void scrollBuffer(char slideDeck[10][10]) {
+  for (int n = 0; n < currDeckSize; n++) {
+    scrollSprite(slideDeck[n]);
+  }
 }
 
-void setup()
-{
-    // Matrix
-    m.init();           // MAX7219 initialization
-    m.setIntensity(15); // initial led matrix intensity, 0-15
+void setup() {
+  // Matrix
+  m.init();            // MAX7219 initialization
+  m.setIntensity(15);  // initial led matrix intensity, 0-15
 
-    // JoyStick
-    pinMode(SwButtonPin, INPUT);     // sets the SW switch as input
-    digitalWrite(SwButtonPin, HIGH); // sets the SW button HIGH
-    Serial.begin(115200);            // sets the baud rate
+  // JoyStick
+  pinMode(SwitchButtonPin, INPUT);      // sets the SW switch as input
+  digitalWrite(SwitchButtonPin, HIGH);  // sets the SW button HIGH
+  Serial.begin(115200);                 // sets the baud rate
 
-    // initialize the LED pin as an output:
-    pinMode(modeLEDPin, OUTPUT);
+  // initialize the LED pin as an output:
+  pinMode(modeLEDPin, OUTPUT);
 
-    // initialize the pushbutton pin as an input:
-    pinMode(pin2Button, INPUT);
-    pinMode(pin3Button, INPUT);
-
-    // Attach an interrupt to the ISR vector
-    attachInterrupt(digitalPinToInterrupt(pin2Button), mode_toggle, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(pin3Button), saveSketch, CHANGE);
+  // Initialize the 2 push buttons to pin2 and pin3.
+  // These 2 pins are the only interrupt ISR vector on UNO.
+  pinMode(pin2Button, INPUT);
+  pinMode(pin3Button, INPUT);
+  attachInterrupt(digitalPinToInterrupt(pin2Button), modeToggle, FALLING);
+  attachInterrupt(digitalPinToInterrupt(pin3Button), saveSketch, FALLING);
 }
 
-void loop()
-{
+void loop() {
+  // mode 0 = sktech mode
+  if (mode == 0) {
+    manage_display();
+  }
 
-    // mode 0 = sktech mode
-    if (mode == 0)
-    {
-        manage_display();
-    }
-
-    // mode 1 = scroll mode
-    else
-    {
-        scrollBuffer(frameBuffer);
-    }
+  // mode 1 = scroll mode
+  else {
+    scrollBuffer(slideDeck);
+  }
 }
